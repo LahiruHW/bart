f4ec1e79bd557e6a0a380db2d7356bf62e55d98a
Lahiru Weliwitiya <lahiruweliwitiya@hotmail.com>
2024-05-16 23:11:52 +0530

    add helper text to find delete button under image picker

--- bart_app/lib/common/widgets/input/image_upload_input.dart
+++ bart_app/lib/common/widgets/input/image_upload_input.dart
@@ -1,10 +1,14 @@
-import 'package:easy_localization/easy_localization.dart';
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:bart_app/common/widgets/image_with_popup.dart';
 import 'package:bart_app/common/utility/bart_image_tools.dart';
 import 'package:bart_app/common/providers/temp_state_provider.dart';
+
+const int MAX_IMAGES = 4;
 
 class BartImagePicker extends StatefulWidget {
   const BartImagePicker({
@@ -44,13 +48,13 @@ class _BartImagePickerState extends State<BartImagePicker> {
     return Consumer<TempStateProvider>(
       builder: (context, provider, child) => GestureDetector(
         // onTap: _imagePaths.length == 4
-        onTap: provider.imagePaths.length == 4
+        onTap: provider.imagePaths.length == MAX_IMAGES
             ? null
             : () => BartImageTools.pickImagesFromGallery().then((imgList) {
                   // final tempLst = [..._imagePaths, ...imgList];
                   final tempLst = [...provider.imagePaths, ...imgList];
-      
-                  if (tempLst.length > 4) {
+
+                  if (tempLst.length > MAX_IMAGES) {
                     ScaffoldMessenger.of(context).showSnackBar(
                       SnackBar(
                         content:
@@ -58,79 +62,96 @@ class _BartImagePickerState extends State<BartImagePicker> {
                             Text(tr('image.picker.snackbar')),
                       ),
                     );
-                    tempLst.removeRange(4, tempLst.length);
+                    tempLst.removeRange(MAX_IMAGES, tempLst.length);
                   }
-      
+
                   // setState(() {
                   //   _imagePaths = tempLst;
                   // });
                   provider.setImagePaths(tempLst);
-
                 }),
-        child: Container(
-          width: double.infinity,
-          // padding: _imagePaths.isEmpty
-          padding: provider.imagePaths.isEmpty
-              ? const EdgeInsets.symmetric(vertical: 45)
-              : const EdgeInsets.symmetric(
-                  vertical: 5,
-                  horizontal: 2.5,
-                ),
-          margin: const EdgeInsets.symmetric(vertical: 10),
-          decoration: BoxDecoration(
-            color:
-                Theme.of(context).colorScheme.surface.computeLuminance() > 0.5
-                    ? Colors.white
-                    : Colors.black,
-            border: Border.all(
-              color: Colors.black.withOpacity(0.2),
-            ),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          // child: _imagePaths.isEmpty
-          child: provider.imagePaths.isEmpty
-              ? Column(
-                  children: [
-                    const Icon(Icons.upload),
-                    const SizedBox(height: 10),
-                    Text(
-                      // "Upload upto 4 pictures of your item you wish to exchange",
-                      context.tr('image.picker.text'),
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                            fontSize: 12,
-                            fontWeight: FontWeight.normal,
-                          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              // padding: _imagePaths.isEmpty
+              padding: provider.imagePaths.isEmpty
+                  ? const EdgeInsets.symmetric(vertical: 45)
+                  : const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 2.5,
                     ),
-                  ],
-                )
-              : SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      // ..._imagePaths.map(
-                      ...provider.imagePaths.map(
-                        (img) => ImageWithPopUpMenu(
-                          imagePath: img,
-                          onDelete: () {
-                            // setState(() => _imagePaths.remove(img));
-                            provider.removeImagePath(img);
-                          },
-                        ),
-                      ),
-                      // if (_imagePaths.length < 4) imgPlaceholder(),
-                      if (provider.imagePaths.length < 4) imgPlaceholder(),
-                    ]
-                        .animate(
-                          delay: 300.ms,
-                          interval: 50.ms,
-                        )
-                        .fadeIn(
-                          duration: 400.ms,
-                          curve: Curves.easeInOutCubic,
-                        ),
-                  ),
+              // margin: const EdgeInsets.symmetric(vertical: 10),
+              margin: const EdgeInsets.only(top: 10),
+              decoration: BoxDecoration(
+                color:
+                    Theme.of(context).colorScheme.surface.computeLuminance() >
+                            0.5
+                        ? Colors.white
+                        : Colors.black,
+                border: Border.all(
+                  color: Colors.black.withOpacity(0.2),
                 ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              // child: _imagePaths.isEmpty
+              child: provider.imagePaths.isEmpty
+                  ? Column(
+                      children: [
+                        const Icon(Icons.upload),
+                        const SizedBox(height: 10),
+                        Text(
+                          // "Upload upto MAX_IMAGES pictures of your item you wish to exchange",
+                          context.tr('image.picker.text'),
+                          textAlign: TextAlign.center,
+                          style:
+                              Theme.of(context).textTheme.titleSmall!.copyWith(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                        ),
+                      ],
+                    )
+                  : SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          // ..._imagePaths.map(
+                          ...provider.imagePaths.map(
+                            (img) => ImageWithPopUpMenu(
+                              imagePath: img,
+                              onDelete: () {
+                                // setState(() => _imagePaths.remove(img));
+                                provider.removeImagePath(img);
+                              },
+                            ),
+                          ),
+                          // if (_imagePaths.length < 4) imgPlaceholder(),
+                          if (provider.imagePaths.length < MAX_IMAGES) imgPlaceholder(),
+                        ]
+                            .animate(
+                              delay: 300.ms,
+                              interval: 50.ms,
+                            )
+                            .fadeIn(
+                              duration: 400.ms,
+                              curve: Curves.easeInOutCubic,
+                            ),
+                      ),
+                    ),
+            ),
+            provider.imagePaths.isNotEmpty
+                ? Container(
+                    margin: const EdgeInsets.only(bottom: 10, left: 2.5),
+                    child: Text(
+                      context.tr('image.picker.see.delete.btn'),
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(fontSize: 14),
+                    ),
+                  )
+                : const SizedBox(height: 10)
+          ],
         ),
       ),
     );
