59b9f5494677deb9176df3f775e4670d45dfa876
Lahiru Weliwitiya <lahiruweliwitiya@hotmail.com>
2024-06-04 18:18:17 +0530

    add double tap to zoom in view-image page

--- bart_app/lib/screens/shared/view_image_page.dart
+++ bart_app/lib/screens/shared/view_image_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
 class ViewImagePage extends StatefulWidget {
@@ -14,17 +15,54 @@ class ViewImagePage extends StatefulWidget {
   State<ViewImagePage> createState() => _ViewImagePageState();
 }
 
-class _ViewImagePageState extends State<ViewImagePage> {
+class _ViewImagePageState extends State<ViewImagePage>
+    with SingleTickerProviderStateMixin {
   late final TransformationController _transformationController;
+
+  late TapDownDetails _doubleTapDetails;
+  late final AnimationController _animationController;
+  late Animation<Matrix4> _animation;
 
   @override
   initState() {
     super.initState();
     _transformationController = TransformationController();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    )..addListener(() {
+        _transformationController.value = _animation.value;
+      });
+  }
+
+  void _handleImageZoom() {
+    Matrix4 endMatrix;
+    Offset position = _doubleTapDetails.localPosition;
+
+    if (_transformationController.value != Matrix4.identity()) {
+      endMatrix = Matrix4.identity();
+    } else {
+      endMatrix = Matrix4.identity()
+        ..translate(-position.dx * 2, -position.dy * 2)
+        ..scale(3.0);
+    }
+
+    _animation = Matrix4Tween(
+      begin: _transformationController.value,
+      end: endMatrix,
+    ).animate(
+      CurveTween(
+        curve: Curves.easeInOutCubic,
+      ).animate(
+        _animationController,
+      ),
+    );
+    _animationController.forward(from: 0);
   }
 
   @override
   dispose() {
+    _animationController.dispose();
     _transformationController.dispose();
     super.dispose();
   }
@@ -56,7 +94,14 @@ class _ViewImagePageState extends State<ViewImagePage> {
             panAxis: PanAxis.free,
             panEnabled: true,
             scaleEnabled: true,
-            child: _buildImage(),
+            minScale: 1.0,
+            maxScale: 5.0,
+            transformationController: _transformationController,
+            child: GestureDetector(
+              onDoubleTapDown: (d) => setState(() => _doubleTapDetails = d),
+              onDoubleTap: _handleImageZoom,
+              child: _buildImage(),
+            ),
           ),
         ),
       ),
