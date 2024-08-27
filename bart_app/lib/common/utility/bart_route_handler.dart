49fd37cce4becb36dcf3dc566f398aaa02082684
Lahiru Weliwitiya <lahiruweliwitiya@hotmail.com>
2024-08-28 04:07:46 +0530

    remove firestore analytics from BartRouteHandler + code cleanup :D

--- bart_app/lib/common/utility/bart_route_handler.dart
+++ bart_app/lib/common/utility/bart_route_handler.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:bart_app/common/providers/temp_state_provider.dart';
-import 'package:bart_app/common/utility/bart_firebase_analytics.dart';
 
 class BartRouteHandler {
+  static final List<String> warningRouteList = [
+    'newItem',
+    'returnItem',
+    'editItem',
+    'editTrade',
+  ];
+
   /// Checks if the current page can be popped based on both
   /// the current location and the last matched location
   static bool canPop(BuildContext context) {
@@ -22,21 +29,21 @@ class BartRouteHandler {
     return canPop;
   }
 
-  static Future<bool?> shouldExitDialog(BuildContext context) {
+  static Future<bool?> showExitDialog(BuildContext context) {
     return showDialog<bool>(
       context: context,
+      // barrierDismissible: false,
+      barrierColor: Colors.black.withOpacity(0.5),
       builder: (context) {
         return AlertDialog(
-          title: const Text('Are you sure?'),
-          content: const Text(
-            'Are you sure you want to leave this page?',
-          ),
+          title: Text(context.tr("pre.exit.dialog.header")),
+          content: Text(context.tr('pre.exit.dialog.body')),
           actions: <Widget>[
             TextButton(
               style: TextButton.styleFrom(
                 textStyle: Theme.of(context).textTheme.labelSmall,
               ),
-              child: const Text('Nevermind'),
+              child: Text(context.tr('no')),
               onPressed: () {
                 Navigator.pop(context, false);
               },
@@ -45,7 +52,7 @@ class BartRouteHandler {
               style: TextButton.styleFrom(
                 textStyle: Theme.of(context).textTheme.labelSmall,
               ),
-              child: const Text('Leave'),
+              child: Text(context.tr('yes')),
               onPressed: () {
                 Navigator.pop(context, true);
               },
@@ -56,11 +63,17 @@ class BartRouteHandler {
     );
   }
 
-  /// all the functions that are done before the app closes
-  static void preExitCallbacks(BuildContext context) {
-    final tempProvider = Provider.of<TempStateProvider>(context, listen: false);
-    tempProvider.clearAllTempData();
-    BartAnalyticsEngine.logAppClose();
+  /// only used in the appbar to check if the exit dialog should be shown
+  static bool shouldShowExitDialog(BuildContext context) {
+    final GoRouter route = GoRouter.of(context);
+    final String location =
+        route.routerDelegate.currentConfiguration.last.matchedLocation;
+    for (final i in warningRouteList) {
+      if (location.endsWith(i)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   static Widget popHandlerWrapper({
@@ -70,17 +83,27 @@ class BartRouteHandler {
     return PopScope(
       canPop: false,
       onPopInvokedWithResult: (didPop, result) async {
-        if (didPop) {
-          return;
-        }
-        final shouldPop =
-            await BartRouteHandler.shouldExitDialog(context) ?? false;
-
-        if (context.mounted && shouldPop) {
-          context.pop();
-        }
+        if (didPop) return;
+        final shouldPop = await preExitWarning(context);
+        if (shouldPop && context.mounted) context.pop();
       },
       child: child,
     );
+  }
+
+  //
+  static Future<bool> preExitWarning(BuildContext context) async {
+    final shouldPop = await BartRouteHandler.showExitDialog(context) ?? false;
+    if (context.mounted && shouldPop) {
+      preExitCallbacks(context);
+      return true;
+    }
+    return false;
+  }
+
+  /// all the functions that are done before the app closes
+  static void preExitCallbacks(BuildContext context) {
+    final tempProvider = Provider.of<TempStateProvider>(context, listen: false);
+    tempProvider.clearAllTempData();
   }
 }
