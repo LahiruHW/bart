7e284f2b5e4cce3cde65ece127e06c590d0bb8dd
Lahiru Weliwitiya <lahiruweliwitiya@hotmail.com>
2024-08-27 05:32:13 +0530

    add date separators when listing trades in home page v2

--- bart_app/lib/common/widgets/home_page_v2_trade_panel.dart
+++ bart_app/lib/common/widgets/home_page_v2_trade_panel.dart
@@ -1,9 +1,10 @@
-import 'package:bart_app/common/widgets/home_trade_item.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 import 'package:bart_app/common/entity/trade.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:bart_app/common/widgets/home_trade_item.dart';
 import 'package:bart_app/common/widgets/shimmer/shimmer_home_trade_item.dart';
-import 'package:go_router/go_router.dart';
 
 class HomePageV2TradePanel extends StatelessWidget {
   const HomePageV2TradePanel({
@@ -20,6 +21,40 @@ class HomePageV2TradePanel extends StatelessWidget {
   final AsyncSnapshot snapshot;
   final int segmentIndex;
   final String userID;
+
+  Widget _dateSeparator(BuildContext context, String date) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: 10.h,
+        bottom: 16.h,
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Divider(
+            thickness: 0.5,
+            color: Theme.of(context).dividerColor,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              height: 20,
+              width: 80,
+              alignment: Alignment.center,
+              color: Theme.of(context).scaffoldBackgroundColor,
+              child: Text(
+                date,
+                style: TextStyle(
+                  color: Theme.of(context).dividerColor,
+                  fontSize: 12.spMin,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,28 +90,57 @@ class HomePageV2TradePanel extends StatelessWidget {
       );
     }
 
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-        (context, index) {
-          final trade = trades[index];
-          return TradeWidget(
-            trade: trade,
-            tradeType: trade.tradeCompType,
-            userID: userID,
-            onTap: () {
-              context.push(
-                '/viewTrade',
-                extra: {
-                  'trade': trades[index],
-                  'tradeType': trade.tradeCompType,
-                  'userID': userID,
-                },
-              );
-            },
-          );
-        },
-        childCount: trades.length,
-      ),
+    final firstTradeDT = trades[0].timeCreated.toDate();
+    final diff = DateTime.now().difference(firstTradeDT).inDays;
+    final date = diff < 1
+        ? "Today"
+        : diff == 1
+            ? "Yesterday"
+            : "${firstTradeDT.day}/${firstTradeDT.month}/${firstTradeDT.year}";
+
+    return MultiSliver(
+      children: [
+        SliverToBoxAdapter(child: _dateSeparator(context, date)),
+        SliverList.separated(
+          itemCount: trades.length,
+          itemBuilder: (context, index) {
+            print(index);
+            final trade = trades[index];
+            return TradeWidget(
+              trade: trade,
+              tradeType: trade.tradeCompType,
+              userID: userID,
+              onTap: () {
+                context.push(
+                  '/viewTrade',
+                  extra: {
+                    'trade': trades[index],
+                    'tradeType': trade.tradeCompType,
+                    'userID': userID,
+                  },
+                );
+              },
+            );
+          },
+          separatorBuilder: (context, index) {
+            final currentDT = DateTime.now();
+            final Trade currentTrade = trades[index];
+            final Trade nextTrade = trades[index + 1];
+            final nextTradeDT = nextTrade.timeCreated.toDate();
+            final showDate = !nextTrade.isSameDayAsTrade(currentTrade);
+            if (showDate) {
+              final diff = currentDT.difference(nextTradeDT).inDays;
+              final date = diff == 0
+                  ? "Today"
+                  : diff == 1
+                      ? "Yesterday"
+                      : "${nextTradeDT.day}/${nextTradeDT.month}/${nextTradeDT.year}";
+              return _dateSeparator(context, date);
+            }
+            return const SizedBox(height: 1);
+          },
+        ),
+      ],
     );
   }
 }
